612e21b6c8b5ede2285fff8593e2d196083964c0
Andreas Halgreen Eiset <eiset@ph.au.dk>
2020-07-10 09:20:52 +0200

    Update setup.R

--- R/setup.R
+++ R/setup.R
@@ -33,9 +33,3 @@ tmps$pckg <- c("Hmisc", "rms", "tidyverse", "lubridate")
 lapply(tmps$pckg, library, character.only = TRUE)
 print(sapply(tmps$pckg, packageVersion))
 
-
-# Setup description -------------------------------------------------------
-#
-# Add functions or other code that are used to setup your files so that all
-# options are in one location, and referenced via a function or `source()` call.
-#
